73040450f24fccc913e75a6423b1d6ed9561f44f
Minh Bui <buileminh342006@gmail.com>
2025-03-23 21:56:40 -0500

    Update

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
-import { Fugaz_One, Inter } from "next/font/google";
+import { Fugaz_One, Open_Sans } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const opensans = Open_Sans({ subsets: ["latin"] });
 const fugaz = Fugaz_One({ subsets: ["latin"], weight: ['400'] });
 
 export const metadata = {
@@ -13,12 +13,12 @@ export default function RootLayout({ children }) {
     const header = (
         <header className="p-4 sm:p-8 flex items-center justify-between
         gap-4">
-            <h1 className={'text-base sm:text-lg ' + fugaz.className}>Broodl</h1>
+            <h1 className={'text-base sm:text-lg textGradient ' + fugaz.className}>Broodl</h1>
         </header>
     )
 
     const footer = (
-        <footer>
+        <footer className="p-4 sm:p-8">
             hi
         </footer>
     )
@@ -26,8 +26,8 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <body
-                className={'w-full max-w-[1000px] mx-auto text-sm sm:text-base min-h-screen flex flex-col '
-                    + inter.className}>
+                className={'w-full max-w-[1000px] mx-auto text-sm sm:text-base min-h-screen flex flex-col text-slate-800 '
+                    + opensans.className}>
                 {header}
                 {children}
                 {footer}
