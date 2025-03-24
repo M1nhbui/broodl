1b93f9ba007a6dc1f375b6e4a13efab3ea0b8557
Minh Bui <buileminh342006@gmail.com>
2025-03-24 01:08:28 -0500

    Update

--- app/layout.js
+++ app/layout.js
@@ -14,12 +14,15 @@ export default function RootLayout({ children }) {
         <header className="p-4 sm:p-8 flex items-center justify-between
         gap-4">
             <h1 className={'text-base sm:text-lg textGradient ' + fugaz.className}>Broodl</h1>
+            <div className="flex items-center justify-between">
+                PLACEHOLDER CTA || STAT
+            </div>
         </header>
     )
 
     const footer = (
-        <footer className="p-4 sm:p-8">
-            hi
+        <footer className="p-4 sm:p-8 grid place-items-center">
+            <p className={'text-indigo-500 ' + fugaz.className}>Created by M1nhbui</p>
         </footer>
     )
 
