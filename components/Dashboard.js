771190dfff85b4822a7c0cd114b747427ef6df38
Minh Bui <buileminh342006@gmail.com>
2025-03-24 21:09:20 -0500

    Update

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Fugaz_One } from "next/font/google";
+import Calendar from './Calendar';
 
 const fugaz = Fugaz_One({ subsets: ["latin"], weight: ['400'] });
 
@@ -10,18 +11,40 @@ export default function Dashboard() {
         date: (new Date()).toDateString()
     }
 
+    const moods = {
+        '&*@#$': '😭',
+        'Sad': '🥲',
+        'Existing': '😶',
+        'Good': '😊',
+        'Elated': '😍',
+    }
+
     return (
-        <div className='flex flex-col flex-1'>
-            <div className='grid grid-cols-1 sm:grid-cols-3 bg-indigo-50 text-indigo-500 rounded-lg '>
+        <div className='flex flex-col flex-1 gap-8 sm:gap-12 md:gap-16'>
+            <div className='grid grid-cols-3 bg-indigo-50 text-indigo-500 p-4 gap-4 rounded-lg '>
                 {Object.keys(statuses).map((status, statusIndex) => {
                     return (
-                        <div key={statusIndex} className='p-4 flex flex-col gap-1 sm:gap-2'>
-                            <p className='font-medium uppercase text-xs sm:text-sm'>{status.replaceAll('_', ' ')}</p>
-                            <p className={'text-base sm:text-lg ' + fugaz.className}>{statuses[status]}</p>
+                        <div key={statusIndex} className=' flex flex-col gap-1 sm:gap-2'>
+                            <p className='font-medium uppercase text-xs sm:text-sm truncate'>{status.replaceAll('_', ' ')}</p>
+                            <p className={'text-base sm:text-lg truncate ' + fugaz.className}>{statuses[status]}</p>
                         </div>
                     )
                 })}
             </div>
+            <h4 className={'text-5xl sm:text-6xl md:text-7xl text-center ' + fugaz.className}>
+                How do you <span className='textGradient'>feel</span> today?
+            </h4>
+            <div className='flex items-stretch flex-wrap gap-4'>
+                {Object.keys(moods).map((mood, moodIndex) => {
+                    return (
+                        <button className={'p-4 px-5 rounded-2xl purpleShadow duration-200 bg-indigo-50 hover:bg-indigo-100 text-center flex flex-col item-center gap-2 flex-1 '} key={moodIndex}>
+                            <p className='text-4xl sm:text-5xl md:text-6xl'>{moods[mood]}</p>
+                            <p className={'text-indigo-500 text-xs sm:text-sm md:text-base ' + fugaz.className}>{mood}</p>
+                        </button>
+                    )
+                })}
+            </div>
+            <Calendar/>
         </div>
     )
 }
